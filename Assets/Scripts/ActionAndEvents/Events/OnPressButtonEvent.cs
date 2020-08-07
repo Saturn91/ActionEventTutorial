8208370e4100bc09c7b070e9c0dc2ea9d58cacb8
Manuel G <manuel.geissberger@gmail.com>
2020-08-07 15:29:06 +0200

    [ADD] KeyPressEvent

---
+++ Assets/Scripts/ActionAndEvents/Events/OnPressButtonEvent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnPressButtonEvent : Event
+{
+    public bool anyKey = false;
+    public string keyValue = "Cancel";
+
+    protected override bool EventTrue()
+    {
+        if (anyKey)
+        {
+            return Input.anyKey;
+        }
+        else
+        {
+            return Input.GetButtonDown(keyValue);
+        }
+    }
+}
