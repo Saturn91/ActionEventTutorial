0479d634b8d04f35626765c8cc8039902ac16e6b
Manuel G <manuel.geissberger@gmail.com>
2020-08-09 14:08:11 +0200

    [ADD] SetTimeAction

---
+++ Assets/Scripts/ActionAndEvents/Actions/SetTimeAction.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetTimeAction : Action
+{
+    public float timeScale = 1;
+
+    public override void OnDo()
+    {
+        Time.timeScale = timeScale;
+    }
+}
