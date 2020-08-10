5e643cdc554ab46363bcaccc1abd80f48e4f43ab
Manuel G <manuel.geissberger@gmail.com>
2020-08-10 07:57:47 +0200

    [ADD] Mainmenu (openSceneAction, CloseGameAction)

---
+++ Assets/Scripts/ActionAndEvents/Actions/OpenSceneAction.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpenSceneAction : Action
+{
+    public string scene = "samplescene";
+
+    public override void OnDo()
+    {
+        try
+        {
+            SceneManager.LoadScene(scene);
+        }
+        catch
+        {
+            Debug.LogError("not able to locate: " + scene);
+        }
+    }
+}
